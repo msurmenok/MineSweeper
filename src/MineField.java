8dc4846a1cb2d06214a91eae6b6b93dee673da6b
Maria Surmenok <mariasurmenok@gmail.com>
2018-09-22 23:16:11 -0700

    created a skeleton of class MineField

---
+++ src/MineField.java
@@ -0,0 +1,21 @@
+
+import java.util.ArrayList;
+
+public class MineField extends ArrayList<ArrayList<Cell>>
+{
+	
+	public MineField()
+	{
+		generateField();
+	}
+	
+	public void generateField()
+	{
+		
+	}
+	
+	int calculateAdjacentMineNumbers(int x, int y)
+	{
+		return 0;
+	}
+}
