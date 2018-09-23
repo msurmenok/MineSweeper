69141dae103043d1e10b28db45d1eddde92e055c
Maria Surmenok <mariasurmenok@gmail.com>
2018-09-22 23:29:23 -0700

    created a skeleton for class Game

---
+++ src/Game.java
@@ -0,0 +1,28 @@
+
+public class Game 
+{
+	private MineField mineField;
+	private Timer timer;
+	private MineCounter mineCounter;
+	private SmileyFace smileyFace;
+	
+	public Game()
+	{
+		
+	}
+	
+	void getUserInput(int x, int y)
+	{
+		
+	}
+	
+	void show()
+	{
+		
+	}
+	
+	void endGame()
+	{
+		
+	}
+}
