a8258b3ca2319d419d425d6330c7039493b40866
Maria Surmenok <mariasurmenok@gmail.com>
2018-09-23 17:30:49 -0700

    Added comments to class Game

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,8 @@
-
+/**
+ * Game class contains and controls all other elements of the game
+ * @author Maria
+ * 
+ */
 public class Game 
 {
 	private MineField mineField;
@@ -6,21 +10,35 @@ public class Game
 	private MineCounter mineCounter;
 	private SmileyFace smileyFace;
 	
+	/**
+	 * Constructs a new game
+	 */
 	public Game()
 	{
 		
 	}
 	
+	/**
+	 * Handles user click on the mine field
+	 * @param x x coordinate of clicked cell
+	 * @param y y coordinate of clicked cell
+	 */
 	void getUserInput(int x, int y)
 	{
 		
 	}
 	
+	/**
+	 * Calls GameViwer class to show GUI to user
+	 */
 	void show()
 	{
 		
 	}
 	
+	/**
+	 * Invoked where game is over. Stops timer and show all mines.
+	 */
 	void endGame()
 	{
 		
