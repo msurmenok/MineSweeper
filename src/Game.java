67e6c7d21360497918b1da6079cd02f3392ddd2b
Maria Surmenok <mariasurmenok@gmail.com>
2018-09-23 22:24:32 -0700

    Created class Game which initialize Model, View, and Controller for our game

---
+++ src/Game.java
@@ -0,0 +1,15 @@
+/**
+ * Creates Model, View, and Controller and starts mainLoop from controller
+ * @author msurmenok
+ *
+ */
+public class Game {
+	void main(String[] args)
+	{
+		/*
+		 * TODO: 
+		 * 1. Create Model, View.
+		 * 2. Create Controller and pass Model and View instances as parameters
+		 */
+	}
+}
