007baa4e01650fa0be383d74e7ac6cf5dc922ab3
Maria Surmenok <mariasurmenok@gmail.com>
2018-09-23 22:06:09 -0700

    changed MineField so it contains 2d array instead of ArrayList

--- src/MineField.java
+++ src/MineField.java
@@ -2,25 +2,41 @@
 import java.util.ArrayList;
 
 /**
- * MineField class is a 2d ArrayList that represents a field of mines
+ * MineField class is a 2d array that represents a field of mines
  * @author msurmenok
  *
  */
-public class MineField extends ArrayList<ArrayList<Cell>>
+public class MineField
 {
+	Cell[][] cells;
+	int height;
+	int width;
+	
 	/**
 	 * Generate mine field for the first time
 	 */
 	public MineField()
 	{
-		generateField();
+		int defaultHeight = 10;
+		int defaultWidth = 20;
+		int numberOfMines = 5;
+		generateField(defaultHeight, defaultWidth, numberOfMines);
 	}
 	
-	public void generateField()
+	/**
+	 * Create mine field and put mines in random cells
+	 */
+	public void generateField(int height, int width, int numberOfMines)
 	{
-		
+		cells = new Cell[height][width];
 	}
 	
+	/**
+	 * Check out how many mines near the specific cell 
+	 * @param x x coordinate of the cell
+	 * @param y y coordinate of the cell
+	 * @return number of mines nearby
+	 */
 	int calculateAdjacentMineNumbers(int x, int y)
 	{
 		return 0;
