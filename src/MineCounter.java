c4ede33e679a3fd8cbcafddbab29a7376c5a61e7
Maria Surmenok <mariasurmenok@gmail.com>
2018-09-24 14:21:19 -0700

    added second * to comment to use comments with javadoc

--- src/MineCounter.java
+++ src/MineCounter.java
@@ -11,13 +11,13 @@ public class MineCounter {
   public MineCounter() {
   }
 
-  /* 
+  /**
   * number of mines is decreased by 1 upon setFlag() 
    */
   private void decreaseMine() {
     numOfMines--;
   }
-  /*
+  /**
    * number of mines is increased by 1 when removeFlag()
    */
   private void increaseMine() {
