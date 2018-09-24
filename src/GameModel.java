85490628232f704b09b8bb16383e57560c22d5cd
Maria Surmenok <mariasurmenok@gmail.com>
2018-09-23 22:04:41 -0700

    Renamed Game class and changed methods for the model

---
+++ src/GameModel.java
@@ -0,0 +1,45 @@
+/**
+ * Game class contains and controls all other elements of the game
+ * @author msurmenok
+ * 
+ */
+public class GameModel 
+{
+	private MineField mineField;
+	private Timer timer;
+	private MineCounter mineCounter;
+	
+	/**
+	 * Constructs a new game
+	 */
+	public GameModel()
+	{
+		
+	}
+	
+	/**
+	 * Reset all variables
+	 */
+	void restartGame()
+	{
+		
+	}
+	
+	/**
+	 * Handles user click on the mine field
+	 * @param x x coordinate of clicked cell
+	 * @param y y coordinate of clicked cell
+	 */
+	void openCell(int x, int y)
+	{
+		
+	}
+	
+	/** 
+	 * If cell is already flagged, remove flag, otherwise set flag
+	 */
+	void toggleCellFlag(int x, int y)
+	{
+		
+	}
+}
