0f9ffb9d79907486b28c5215288890f4d72d3da4
Maria Surmenok <mariasurmenok@gmail.com>
2018-09-23 17:35:29 -0700

    Added comments to the class MineField

--- src/MineField.java
+++ src/MineField.java
@@ -1,9 +1,16 @@
 
 import java.util.ArrayList;
 
+/**
+ * MineField class is a 2d ArrayList that represents a field of mines
+ * @author msurmenok
+ *
+ */
 public class MineField extends ArrayList<ArrayList<Cell>>
 {
-	
+	/**
+	 * Generate mine field for the first time
+	 */
 	public MineField()
 	{
 		generateField();
