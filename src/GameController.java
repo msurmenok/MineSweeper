bb0bea96366f45c1599a57aae165ced306b2bf56
Maria Surmenok <mariasurmenok@gmail.com>
2018-09-23 22:17:16 -0700

    Created GameController

---
+++ src/GameController.java
@@ -0,0 +1,32 @@
+import java.util.Queue;
+
+/**
+ * Controls the game
+ * @author msurmenok
+ *
+ */
+public class GameController {
+	Queue messageQueue;
+	GameModel model;
+	GameView view;
+	
+	/**
+	 * Initialize game controller
+	 */
+	public GameController(GameView view, GameModel model)
+	{
+		this.view = view;
+		this.model = model;
+		// TODO: create field messageQueue in GameView
+		//this.messageQueue = view.messageQueue;
+	}
+	
+	/**
+	 * Iterates through messageQueue and updates the model and the view
+	 */
+	void mainLoop()
+	{
+		
+	}
+	
+}
