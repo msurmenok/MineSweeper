4588b3bc4f8dc761f9012256fa229036ff61cafa
joymyhome <faith.zjing@gmail.com>
2018-11-20 11:50:42 -0800

    Modified cell to the Inhee's version

--- src/Cell.java
+++ src/Cell.java
@@ -1,50 +1,36 @@
-/**
- * Cell is a unit on the mine field that stores all kinds of cell.
- * @author jing
- *
- */
 public class Cell {
-	private boolean flagged;
-	private boolean open;
-	private boolean mine;
-	
-	/**
-	 * By default, a cell is not flagged and is not visible when initiate at the beginning.
-	 */
-	public Cell() {
-		flagged = false;
-		open = false;
+	private int adjacentMines;
+
+	private boolean hasFlagSet;
+
+	private boolean isOpened;
+
+	public Cell(int adjacentMines) {
+		this.adjacentMines = adjacentMines;
+		hasFlagSet = false;
+		isOpened = false;
 	}
-	/**
-	 * The method to flag a cell
-	 */
-	public void setFlag() {
-		flagged = true;
+
+	public int adjacentMines() {
+		return this.adjacentMines;
 	}
-	
-	/**
-	 * The method to unflag a cell
-	 */
-	public void removeFlag() {
-		flagged = false;
+
+	public boolean toggleFlag() {
+		// hasFlagSet => unset Flag (i.e. !hasFlagSet)
+		// !hasFlagSet => set Flag (i.e. hasFlagSet)
+		hasFlagSet = !hasFlagSet;
+		return hasFlagSet;
 	}
-	
-	/**
-	 * The method to open a cell
-	 */
-	public void openCell() {
-		open = true;
-	}
-	
-	public boolean isOpen() {
-		return this.open;
-	}
-	
+
 	public boolean isFlagged() {
-		return this.flagged;
+		return hasFlagSet;
 	}
-	
-	public boolean isMine() {
-		return this.mine;
+
+	public void setOpen() {
+		isOpened = true;
+	}
+
+	public boolean isOpened() {
+		return this.isOpened;
 	}
 }
