982f96c5b6a20803c0658f192789d632f65b7063
joymyhome <faith.zjing@gmail.com>
2018-11-22 12:49:42 -0800

    add updateGameInfo()  in controller

--- src/edu/sjsu/cs/cs151/controller/Controller.java
+++ src/edu/sjsu/cs/cs151/controller/Controller.java
@@ -6,19 +6,19 @@ import java.util.Queue;
 import java.util.concurrent.BlockingQueue;
 
 import edu.sjsu.cs.cs151.Message;
+import edu.sjsu.cs.cs151.model.Cell;
 import edu.sjsu.cs.cs151.model.Model;
 import edu.sjsu.cs.cs151.view.View;
 
 /**
  * Controls the game
  * 
- * @author msurmenok
- *
  */
 public class Controller {
 	BlockingQueue<Message> queue;
 	Model model;
 	View view;
+	GameInfo gameInfo;
 	private List<Valve> valves = new LinkedList<Valve>();
 
 	/**
@@ -33,6 +33,8 @@ public class Controller {
 		this.view = view;
 		this.model = model;
 		this.queue = queue;
+		this.gameInfo = new GameInfo(model);
+		
 	}
 
 	/**
@@ -40,6 +42,30 @@ public class Controller {
 	 */
 	public void mainLoop() {
 
+	}
+	
+	/**
+	 * update Gameinfo
+	 * closed cell: -2
+	 * mine cell: -1
+	 * opened empty cell: 0
+	 * open cell with surrounding mine: adjacentMnes
+	 * flagged cell: 10
+	 */
+	public void updateGameInfo() {
+		Cell[][] currentCells = model.getMineField().getCell();//get the cells info from model
+		int h = model.getHeight();
+		int w = model.getWidth();
+		for (int i = 0; i < h; i++) {
+			for (int j = 0; j < w; j++) {
+				if (currentCells[i][j].isFlagged()) { //if the cells has been flagged
+					gameInfo.gameInfoUpdate(i, j, gameInfo.flag()); //change gameinfo Matrix
+				} else {
+					int currCell = currentCells[i][j].adjacentMines();
+					gameInfo.gameInfoUpdate(i, j, currCell);
+				}
+			}
+		}
 	}
 
 	/**
