19e0cc1eaf4aeb028b0562d216660da0f8fbf64c
Maria Surmenok <mariasurmenok@gmail.com>
2018-11-21 19:48:21 -0800

    added function execute to interface Valve

--- src/edu/sjsu/cs/cs151/controller/Valve.java
+++ src/edu/sjsu/cs/cs151/controller/Valve.java
@@ -1,5 +1,14 @@
 package edu.sjsu.cs.cs151.controller;
 
+import edu.sjsu.cs.cs151.Message;
+
 public interface Valve {
 
+	/**
+	 * Performs certain action in response to message
+	 * 
+	 * @param message
+	 * @return
+	 */
+	public ValveResponse execute(Message message);
 }
