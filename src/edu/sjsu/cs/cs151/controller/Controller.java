b684c7735977d211f1de776facbb23722bff7aac
joymyhome <faith.zjing@gmail.com>
2018-11-22 13:00:28 -0800

    Formatted

--- src/edu/sjsu/cs/cs151/controller/Controller.java
+++ src/edu/sjsu/cs/cs151/controller/Controller.java
@@ -34,7 +34,7 @@ public class Controller {
 		this.model = model;
 		this.queue = queue;
 		this.gameInfo = new GameInfo(model);
-		
+
 	}
 
 	/**
@@ -43,23 +43,19 @@ public class Controller {
 	public void mainLoop() {
 
 	}
-	
+
 	/**
-	 * update Gameinfo
-	 * closed cell: -2
-	 * mine cell: -1
-	 * opened empty cell: 0
-	 * open cell with surrounding mine: adjacentMnes
-	 * flagged cell: 10
+	 * update GameInfo closed cell: -2 mine cell: -1 opened empty cell: 0 open cell
+	 * with surrounding mine: adjacentMnes flagged cell: 10
 	 */
 	public void updateGameInfo() {
-		Cell[][] currentCells = model.getMineField().getCell();//get the cells info from model
+		Cell[][] currentCells = model.getMineField().getCell();// get the cells info from model
 		int h = model.getHeight();
 		int w = model.getWidth();
 		for (int i = 0; i < h; i++) {
 			for (int j = 0; j < w; j++) {
-				if (currentCells[i][j].isFlagged()) { //if the cells has been flagged
-					gameInfo.gameInfoUpdate(i, j, gameInfo.flag()); //change gameinfo Matrix
+				if (currentCells[i][j].isFlagged()) { // if the cells has been flagged
+					gameInfo.gameInfoUpdate(i, j, gameInfo.flag()); // change gameinfo Matrix
 				} else {
 					int currCell = currentCells[i][j].adjacentMines();
 					gameInfo.gameInfoUpdate(i, j, currCell);
