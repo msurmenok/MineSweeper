e293bb97c4cab07e3471162338da30696b5612f0
Maria Surmenok <mariasurmenok@gmail.com>
2018-11-22 13:16:46 -0800

    added control panel to GUI

--- src/edu/sjsu/cs/cs151/view/View.java
+++ src/edu/sjsu/cs/cs151/view/View.java
@@ -1,24 +1,49 @@
 package edu.sjsu.cs.cs151.view;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 import edu.sjsu.cs.cs151.Message;
 import edu.sjsu.cs.cs151.model.Model;
 
 import java.util.Queue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * Class that will render visual representation of the game
  */
-public class View extends JFrame {
+public class View {
 
 	BlockingQueue<Message> queue;
 	Model model;
 
 	// constructor
-	private View(BlockingQueue<Message> queue) {
+	public View(BlockingQueue<Message> queue) {
 		this.queue = queue;
+
+		// Replace with information obtained from... I don't know, from Game class?
+		int numberOfMines = 4;
+
+		// Top row that holds mine counter, new game button, and timer
+		JPanel controlPanel = new JPanel();
+		// Specify that elements should be from left to right
+		BoxLayout boxLayout = new BoxLayout(controlPanel, BoxLayout.X_AXIS);
+		controlPanel.setLayout(boxLayout);
+		// Create elements for control panel
+		JLabel mineCounter = new JLabel("" + numberOfMines);
+		JButton newGameButton = new JButton("New Game");
+		JLabel timer = new JLabel("00:00");
+		controlPanel.add(mineCounter);
+		controlPanel.add(newGameButton);
+		controlPanel.add(timer);
+
+		JFrame frame = new JFrame("MineSweeper");
+		frame.add(controlPanel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
 	}
 
 	/**
@@ -33,4 +58,8 @@ public class View extends JFrame {
 	public static View init(BlockingQueue<Message> queue) {
 		return new View(queue);
 	}
+
+	public void run() {
+	}
+
 }
