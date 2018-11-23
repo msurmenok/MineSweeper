da786f1fbfa81377a9dab74e5e49cbef029913e8
Maria Surmenok <mariasurmenok@gmail.com>
2018-11-22 19:46:29 -0800

    added parameters width and height to store the position of clicked button

--- src/edu/sjsu/cs/cs151/RightClickMessage.java
+++ src/edu/sjsu/cs/cs151/RightClickMessage.java
@@ -1,5 +1,11 @@
 package edu.sjsu.cs.cs151;
 
 public class RightClickMessage extends Message {
+	int height;
+	int width;
 
+	public RightClickMessage(int height, int width) {
+		this.height = height;
+		this.width = width;
+	}
 }
