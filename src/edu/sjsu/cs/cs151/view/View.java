108e76781dc5365b8997fb4054b8829dc7385dfb
Maria Surmenok <mariasurmenok@gmail.com>
2018-11-22 18:48:42 -0800

    Added MouseListener for every button and print button row and column on Console

--- src/edu/sjsu/cs/cs151/view/View.java
+++ src/edu/sjsu/cs/cs151/view/View.java
@@ -1,6 +1,8 @@
 package edu.sjsu.cs.cs151.view;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
 import java.awt.*;
 import java.awt.event.*;
 
@@ -20,6 +22,8 @@ public class View extends JFrame {
 
 	BlockingQueue<Message> queue;
 	Model model;
+	int numberOfColumns;
+	int numberOfRows;
 
 	// constructor
 	public View(BlockingQueue<Message> queue) {
@@ -27,9 +31,11 @@ public class View extends JFrame {
 
 		// Replace with information from GameInfo
 		int numberOfMines = 4;
-		int height = 18;
-		int width = 18;
+		int numberOfRows = 8;
+		int numberOfColumns = 8;
 
+		this.numberOfColumns = numberOfColumns;
+		this.numberOfRows = numberOfRows;
 		// CONTROLPANEL
 		// Top row that holds mine counter, new game button, and timer
 		JPanel controlPanel = new JPanel();
@@ -48,11 +54,14 @@ public class View extends JFrame {
 		// Panel for a mine field
 		JPanel fieldPanel = new JPanel();
 		fieldPanel.setSize(200, 500);
-		fieldPanel.setLayout(new GridLayout(height, width));
-		for (int i = 0; i < height * width; i++) {
+		fieldPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+		fieldPanel.setLayout(new GridLayout(numberOfRows, numberOfColumns));
+		for (int i = 0; i < numberOfRows * numberOfColumns; i++) {
 			JButton cell = new JButton();
+
+			cell.setName(i + ", ");
 			cell.setPreferredSize(new Dimension(30, 30));
-			// add mouseListener here
+			cell.addMouseListener(new MineFieldListener());
 			fieldPanel.add(cell);
 		}
 
@@ -77,8 +86,6 @@ public class View extends JFrame {
 		return new View(queue);
 	}
 
-	// Inner class to handle right-click and left-click on cell button
-
 	// Inner class to handle click on NewGameButton
 	private class NewGameListener implements ActionListener {
 		@Override
@@ -91,6 +98,50 @@ public class View extends JFrame {
 				exception.printStackTrace();
 			}
 
+		}
+
+	}
+
+	// Inner class to handle right-click and left-click on cell button
+	private class MineFieldListener implements MouseListener {
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			// Get id of clicked number
+			String buttonInfo = e.getSource().toString();
+			buttonInfo = buttonInfo.replaceFirst("javax.swing.JButton\\[", "");
+			buttonInfo = buttonInfo.split(",")[0];
+			int buttonNumber = Integer.parseInt(buttonInfo);
+			
+			// Calculate row and column of the button
+			int row = (int) buttonNumber / numberOfColumns;
+			int column = buttonNumber - (row * numberOfColumns);
+			System.out.println("Button with row: " + row + ", column: " + column);
+			
+			// Check if it was right or left click. Create appropriate Message for message queue
+			if (SwingUtilities.isRightMouseButton(e)) {
+				// Create RightClickMessage here
+				System.out.println("Right button clicked");
+			} else {
+				// Create LeftClickMessage here
+				System.out.println("Left button clicked");
+			}
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
 		}
 
 	}
