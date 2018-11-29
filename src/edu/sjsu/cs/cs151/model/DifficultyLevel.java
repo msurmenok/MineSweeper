6d59099170f5494e3e78e905642831879b101fb3
joymyhome <faith.zjing@gmail.com>
2018-11-28 23:41:57 -0800

    adding difficulty level-easy, medium and hard

---
+++ src/edu/sjsu/cs/cs151/model/DifficultyLevel.java
@@ -0,0 +1,11 @@
+package edu.sjsu.cs.cs151.model;
+
+public class DifficultyLevel {
+	final static int EASY = 8;
+	final static int MEDIUM = 10;
+	final static int HARD = 12;
+	final static int EASY_MINES = 10;
+	final static int MEDIUM_MINES = 20;
+	final static int HARD_MINES = 30;
+	
+}
