44182ac76e6bf5911d3147dd813634a7b301c6a2
Maria Surmenok <mariasurmenok@gmail.com>
2018-12-04 11:17:42 -0800

    changed modifier to public for Difficulty variables

--- src/edu/sjsu/cs/cs151/model/DifficultyLevel.java
+++ src/edu/sjsu/cs/cs151/model/DifficultyLevel.java
@@ -1,11 +1,11 @@
 package edu.sjsu.cs.cs151.model;
 
 public class DifficultyLevel {
-	final static int EASY = 8;
-	final static int MEDIUM = 10;
-	final static int HARD = 12;
-	final static int EASY_MINES = 10;
-	final static int MEDIUM_MINES = 20;
-	final static int HARD_MINES = 30;
+	public final static int EASY = 8;
+	public final static int MEDIUM = 10;
+	public final static int HARD = 12;
+	public final static int EASY_MINES = 10;
+	public final static int MEDIUM_MINES = 20;
+	public final static int HARD_MINES = 30;
 	
 }
