07a6f631014a45c3f0e2a907b8b1c0ea5162efb0
joymyhome <faith.zjing@gmail.com>
2018-12-04 15:51:54 -0800

    modified difficulty levels

--- src/edu/sjsu/cs/cs151/model/DifficultyLevel.java
+++ src/edu/sjsu/cs/cs151/model/DifficultyLevel.java
@@ -4,8 +4,8 @@ public class DifficultyLevel {
 	public final static int EASY = 8;
 	public final static int MEDIUM = 10;
 	public final static int HARD = 12;
-	public final static int EASY_MINES = 10;
-	public final static int MEDIUM_MINES = 20;
-	public final static int HARD_MINES = 30;
+	public final static int EASY_MINES = 8;
+	public final static int MEDIUM_MINES = 16;
+	public final static int HARD_MINES = 24;
 	
 }
